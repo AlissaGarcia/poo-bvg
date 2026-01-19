e40eb46934df63d3324e122f90e55ea85577280b
Alissa Garcia <118315759+AlissaGarcia@users.noreply.github.com>
2026-01-19 18:08:37 +0000

    criação de Aluno

--- Projeto_5/Projeto_5 - Alissa Garcia/src/Aluno.cpp
+++ Projeto_5/Projeto_5 - Alissa Garcia/src/Aluno.cpp
@@ -1,1 +1,29 @@
 //Código de Alissa Garcia ADS S3
+
+#include "Aluno.h"
+
+//Implementação da Classe
+
+// Construtor padrão
+    Aluno::Aluno() :  Usuario() { //herança
+        this->matricula  = "NULL";
+        this->curso = "NULL";
+        //Deixei tudo nulo por default
+    }
+
+// Construtor parametrizado
+    Aluno::Aluno(string nome, string email, string tipo, string matricula, string curso) : Usuario(nome, email, tipo){ //herança
+        this->matricula  = matricula;
+        this->curso = curso;
+        
+    }
+
+
+// Metodo de impressao das informações com sobrescrita:
+    void Aluno::gerarRelatorio(){
+        Usuario::gerarRelatorio(); //chamando a classe base
+        std::cout << "A matrícula do Usuário é: " << this->matricula << endl;
+        std::cout << "O curso é: " << this->curso << endl;
+
+        
+    }
--- Projeto_5/Projeto_5 - Alissa Garcia/src/Aluno.h
+++ Projeto_5/Projeto_5 - Alissa Garcia/src/Aluno.h
@@ -1,1 +1,24 @@
 //Código de Alissa Garcia ADS S3
+
+#ifndef ALUNO_H
+#define ALUNO_H
+
+#include "Usuario.h"
+#include <string>
+#include <iostram>
+
+using namespace std;
+
+class Aluno : public Usuario {
+    private:
+        string matricula;
+        string curso; //atributos adicionais
+    public:
+    Aluno(); //construtor padrão
+    Aluno(string nome, string email, string tipo, string matricula, string curso); //Cosntrutor parametrizado
+
+    void gerarRelatorio() override; //método sobrescrito
+
+};
+
+#endif //ALUNO_H
