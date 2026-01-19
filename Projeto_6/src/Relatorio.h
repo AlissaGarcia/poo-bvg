946edeea9f22cf5b8155304840eebbd21bc3b9d8
Alissa Garcia <118315759+AlissaGarcia@users.noreply.github.com>
2026-01-19 20:08:50 +0000

    Criação de Arquivos e transferências do projeto anterior

---
+++ Projeto_6/src/Relatorio.h
@@ -0,0 +1,12 @@
+#ifndef RELATORIO_H
+#define RELATORIO_H
+
+class Relatorio {
+public:
+    virtual ~Relatorio() = default;
+
+    virtual void gerarRelatorio() const = 0; // Método virtual puro
+};
+
+#endif // RELATORIO_H
+```
---
+++ Projeto_6/src/UsuarioAutenticavel.h
@@ -0,0 +1,9 @@
+//Código de Alissa Garcia ADS - S3 
+
+#ifndef USUARIO_AUTENTICAVEL_H
+#define USUARIO_AUTENTICAVEL_H
+
+#include <string>
+
+#endif // USUARIO_AUTENTICAVEL_H
+```
