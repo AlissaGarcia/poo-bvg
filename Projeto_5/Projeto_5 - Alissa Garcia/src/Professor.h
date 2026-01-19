c638aff8005ed4fc672ec61144c44e0a23c96db9
Alissa Garcia <118315759+AlissaGarcia@users.noreply.github.com>
2026-01-19 18:21:56 +0000

    Implementação do Professor

--- Projeto_5/Projeto_5 - Alissa Garcia/src/Professor.h
+++ Projeto_5/Projeto_5 - Alissa Garcia/src/Professor.h
@@ -1,2 +1,24 @@
 //Código de Alissa Garcia ADS S3
 
+#ifndef PROFESSOR_H
+#define PROFESSOR_H
+
+#include "Usuario.h"
+#include <string>
+#include <iostram>
+
+using namespace std;
+
+class Professor : public Usuario {
+    private:
+        string areadeAtuacao;
+       vector<string> disciplinasMinistradas; //atributos adicionais
+    public:
+    Professor(); //construtor padrão
+    Professor(string nome, string email, string tipo, string areadeAtuacao); //Cosntrutor parametrizado
+    void disciplinasAdicionadas(string disciplina); //vetor de strings
+    void gerarRelatorio() override; //método sobrescrito
+
+};
+
+#endif //PROFESSOR_H
