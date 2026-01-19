4f03daf407b697fc935f953fff47a20d72e5fd73
Alissa Garcia <118315759+AlissaGarcia@users.noreply.github.com>
2026-01-19 18:27:45 +0000

    Implementação de FuncionarioAdministrativo

--- Projeto_5/Projeto_5 - Alissa Garcia/src/FuncionarioAdministrativo.cpp
+++ Projeto_5/Projeto_5 - Alissa Garcia/src/FuncionarioAdministrativo.cpp
@@ -1,1 +1,24 @@
 //Código de Alissa Garcia ADS S3
+
+#ifndef FUNCIONARIO_ADM_H
+#define FUNCIONARIO_ADM_H
+
+#include "Usuario.h"
+#include <string>
+#include <iostram>
+
+using namespace std;
+
+class Funcionario_adm : public Usuario {
+    private:
+        string departamento;
+        string cargo; //atributos adicionais
+    public:
+    Funcionario_adm(); //construtor padrão
+    Funcionario_adm(string nome, string email, string tipo, string departamento, string cargo); //Cosntrutor parametrizado
+
+    void gerarRelatorio() override; //método sobrescrito
+
+};
+
+#endif //FUNCIONARIO_ADM_H
