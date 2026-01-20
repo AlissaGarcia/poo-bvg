ca3d038d37cc8ba69bc5fb99506e0f0c6f6016b4
Alissa Garcia <118315759+AlissaGarcia@users.noreply.github.com>
2026-01-20 18:29:48 +0000

    Implementação de Usuário Autenticável

--- Projeto_6/src/Usuario.h
+++ Projeto_6/src/Usuario.h
@@ -6,6 +6,7 @@
 #include <string>
 #include <iostream>
 
+
 using namespace std;
 
 //Declaração da Classe 
--- Projeto_6/src/UsuarioAutenticavel.h
+++ Projeto_6/src/UsuarioAutenticavel.h
@@ -4,6 +4,23 @@
 #define USUARIO_AUTENTICAVEL_H
 
 #include <string>
+#include "Usuario.h"
+
+class UsuarioAutenticavel : public Usuario {
+protected:
+    std::string senha;
+
+public:
+    UsuarioAutenticavel();
+    UsuarioAutenticavel(std::string nome, std::string email, std::string tipo, std::string senha);
+    virtual ~UsuarioAutenticavel() = default;
+
+    // Método abstrato para autenticação
+    virtual bool autenticar(std::string senha) const = 0;
+
+    // Setter para senha (pode ser usado para alterar senha)
+    void setSenha(std::string novaSenha);
+};
 
 #endif // USUARIO_AUTENTICAVEL_H
 
